b37ed1e24b37e06e22c021649bde37fbe526fdd2
adrian-brace <adrian.brace@btinternet.com>
2019-01-04 18:58:58 +0000

    Ranking update completed
    Ranking update completed

--- public/scripts/shared_functions.js
+++ public/scripts/shared_functions.js
@@ -156,7 +156,7 @@ function showRankingUpdateDue() {
 	var today = new Date();
 	var month = today.getMonth();
 	var day = today.getDate();
-	return (month == 11 && day > SPRING_START_DAY) || (month === 1 && day < 7);
+	return (month == 11 && day > SPRING_START_DAY) || (month === 1 && day < 4);
 }
 
 function getMasterConfiguration(http, xmlToJson, getClubNameOnly){
